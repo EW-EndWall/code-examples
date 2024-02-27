aaac560972732945234826407cba6af03b43e0c9
EndWall <43109779+EW-EndWall@users.noreply.github.com>
2024-02-27 16:07:28 +0300

    update / remove and move
    https://github.com/EW-EndWall/example-estate ,
    https://github.com/EW-EndWall/example-personal

--- example-projects/bootstrap-vanilla/example-personal/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function () {
-  // remove display none
-  document.body.style = "";
-
-  document.getElementById("currentDate").textContent +=
-    new Date().getFullYear();
-  // Initiate the wowjs
-  new WOW().init();
-});
